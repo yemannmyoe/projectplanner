a908a6604d67286958ac1b8c6b06d2345eff614a
yemann myoe <yemannmyoe1876@gmail.com>
2024-06-08 01:52:29 +0700

    filter nav active link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="home" >
    <h1>Home</h1>
+   <FilterNav @filterValue="current=$event" :current="current"></FilterNav>
    <div v-for="project in projects" :key="project.id">
     <SingleProject :project="project" @delete="deleteProject" @complete="completeProject"></SingleProject>
   </div>
   </div>
+  {{ current }}
 </template>
 
 <script>
 // @ is an alias to /src
 
 import SingleProject from '../components/SingleProject.vue';
-
+import FilterNav from '../components/FilterNav.vue';
 export default {
   name:'Home',
   components:{
     SingleProject,
+    FilterNav
   },
   data(){
     return{
-      projects:[] // Initial empty array for projects
+      projects:[],// Initial empty array for projects
+      current:"all"
     }
   },
 
